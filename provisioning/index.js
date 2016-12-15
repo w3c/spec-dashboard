5da57dc774de4e013d60677abaef0bac3c8039c0
Dominique Hazael-Massieux <dom@w3.org>
2016-12-15 16:10:11 +0100

    Handle specs developed by multiple groups

--- provisioning/index.js
+++ provisioning/index.js
@@ -9,9 +9,27 @@ w3c.apiKey = config.w3capikey;
 const relevantSpecStatus = status => ['Retired', 'Group Note', 'Recommendation'].indexOf(status) === -1
 const last = a => a[a.length - 1];
 const specDate = s => last(s._links['latest-version'].href.split('/'));
+const groupId = g => last(g.href.split('/'));
 const specDateSorter = (s1, s2) => specDate(s2) - specDate(s1);
 
+const preferGroup = (preferedGroup, otherGroup) => {
+    return (g1, g2) => {if (preferedGroup == g1 && otherGroup == g2) { return -1;} else if (g1 == otherGroup && preferedGroup == g2) { return 1;} else { return 0;}};
+};
+const sharedSpecGroupSorter = (gid1, gid2) => {
+    for (let prefers =  [
+        // Prefer WebRTC to DAS
+        [47318,43696]
+    ], i = 0; i < prefers.length; i++) {
+        const gs = prefers[i];
+        const cmp = preferGroup(gs[0], gs[1])(gid1, gid2);
+        if (cmp !== 0) return cmp;
+    }
+    return gid2 - gid1;
+};
+const sharedSpecGroupPicker = (wg, groupList) => !groupList || groupList.map(g => groupId(g)).sort(sharedSpecGroupSorter)[0] == wg.id;
+
 w3c.groups().fetch({embed:true}, (err, groups) => {
+    if (err) return console.error(err);
     const workinggroups = groups.filter(g => g.type === 'working group');
     async.map(workinggroups, (wg,cb) => {
         w3c.group(wg.id).specifications().fetch({embed: true}, (err, specs) => {
@@ -21,7 +39,18 @@ w3c.groups().fetch({embed:true}, (err, groups) => {
             if (oldSpecs.length) {
                 console.error("The following " + wg.name + " specs have not been touched in 3 years:\n" + oldSpecs.map(s => "* "  + s.title + " (" + specDate(s) + ")").join("\n"));
             }
-            createSpreadSheet(wg, unfinishedSpecs.map(s=> s.shortlink), cb);
+            async.filter(unfinishedSpecs,
+                         (s, filtercb) => {
+                             w3c.specification(s.shortname).version(specDate(s)).deliverers().fetch((err, groups) => {
+                                 //if (err) return filtercb(err);
+                                 filtercb(null, sharedSpecGroupPicker(wg, groups));
+                             }
+                             );
+                         },
+                         (err, ownedSpecs) => {
+                             if (err) console.error(err);
+                             createSpreadSheet(wg, ownedSpecs.map(s=> s.shortlink), cb);
+                         });
         });
     }, (err, results) => {
         if (err) return console.error(err);
