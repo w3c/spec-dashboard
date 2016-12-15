7abca0e22b28c69fe80e2f19a3eda60ed2900e46
Dominique Hazael-Massieux <dom@w3.org>
2016-12-15 14:26:29 +0100

    Save list of created spreadsheets in a JSON file

--- provisioning/index.js
+++ provisioning/index.js
@@ -1,5 +1,7 @@
 const config = require("../config.json"),
       request = require('request'),
+      async = require('async'),
+      fs = require('fs'),
       w3c = require('node-w3capi');
 
 w3c.apiKey = config.w3capikey;
@@ -11,20 +13,25 @@ const specDateSorter = (s1, s2) => specDate(s2) - specDate(s1);
 
 w3c.groups().fetch({embed:true}, (err, groups) => {
     const workinggroups = groups.filter(g => g.type === 'working group');
-    workinggroups.forEach(wg => {
+    async.map(workinggroups, (wg,cb) => {
         w3c.group(wg.id).specifications().fetch({embed: true}, (err, specs) => {
             const unfinishedSpecs = specs.filter(s => s._links ? !s._links['superseded-by'] && relevantSpecStatus(s._links['latest-version'].title) : console.error(s))
                   .sort(specDateSorter);
-            createSpreadSheet(wg, unfinishedSpecs.map(s=> s.shortlink));
             const oldSpecs = unfinishedSpecs.filter(s => specDate(s) <= "20131215");
             if (oldSpecs.length) {
-                console.error("The following " + wg.title + " specs have not been touched in 3 years:\n" + oldSpecs.map(s => "* "  + s.title + " (" + specDate(s) + ")").join("\n"));
+                console.error("The following " + wg.name + " specs have not been touched in 3 years:\n" + oldSpecs.map(s => "* "  + s.title + " (" + specDate(s) + ")").join("\n"));
             }
+            createSpreadSheet(wg, unfinishedSpecs.map(s=> s.shortlink), cb);
         });
+    }, (err, results) => {
+        if (err) return console.error(err);
+        const groups = results.filter(g => g).reduce((a,b) => { a[b.id] = {name: b.name, url: b.url}; return a;}, {});
+        fs.writeFileSync("./groups.json", JSON.stringify(groups, null, 2));
     });
 });
 
-function createSpreadSheet(wg, specs) {
+
+function createSpreadSheet(wg, specs, cb) {
     if (specs.length) {
         const body = "TR shortlink,FPWD,Wide Review end, CR, Test Suite status, PR, PER, Rec, Comments & notes\n" + specs.join("\n");
         request({
@@ -35,9 +42,10 @@ function createSpreadSheet(wg, specs) {
             },
             body: body
         }, function (error, response, body) {
-            console.log(wg.name, config.ethercalc + body);
+            cb(null, {id: wg.id, name: wg.name, url: config.ethercalc + body});
         });
     } else {
         console.error(wg.name + " has no active spec to track");
+        cb(null);
     }
 }
