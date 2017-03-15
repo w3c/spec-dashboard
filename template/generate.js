c2ae2501d666bc9335c3a1285abfbdbfbad22c4b
Dominique Hazael-Massieux <dom@w3.org>
2017-03-15 11:46:02 +0100

    Wrap content into a template

---
+++ template/generate.js
@@ -0,0 +1,47 @@
+var sections = [];
+var hero = [];
+var sectionsOrig = document.querySelectorAll("section");
+for (var i = 0; i < sectionsOrig.length; i++) {
+    sections.push(sectionsOrig[i].cloneNode(true));
+}
+var heroOrig = document.querySelectorAll("header > *");
+for (var i = 0; i < heroOrig.length; i++) {
+    hero.push(heroOrig[i].cloneNode(true));
+}
+var styleOrig = document.querySelector("style");
+if (styleOrig) {
+    styleOrig = styleOrig.cloneNode(true);
+}
+if (document.getElementById("toc")) {
+    var tocOrig = document.getElementById("toc").cloneNode(true);
+}
+
+var scripts = ['https://w3c.github.io/mediartc-roadmap-ui/assets/js/sidenav.js'];
+
+var templateXhr = new XMLHttpRequest();
+templateXhr.responseType = 'text';
+templateXhr.open("GET", "template/page");
+templateXhr.onload = function() {
+    document.documentElement.innerHTML = this.responseText;
+    if (styleOrig) {
+        document.querySelector('head').appendChild(styleOrig);
+    }
+    for (var i = 0 ; i < sections.length ; i++) {
+        document.querySelector('.main-content .container').appendChild(sections[i]);
+    }
+    for (var i = 0 ; i < hero.length ; i++) {
+        document.querySelector('.hero .container').appendChild(hero[i]);
+    }
+    for (var i = 0 ; i < scripts.length ; i++) {
+        var s = document.createElement("script");
+        s.src = scripts[i];
+        document.querySelector('body').appendChild(s);
+    }
+
+    document.querySelector('title').textContent = hero[0].textContent;
+    if (tocOrig) {
+        var nav = document.querySelector("aside nav");
+        nav.appendChild(tocOrig);
+    }
+}
+templateXhr.send();
